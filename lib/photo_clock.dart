a09d019e6de6321661a1421a9111bc81250813b8
Danny Peak <dannypeaky@gmail.com>
2024-12-19 12:26:52 +0000

    Scrape photos from google photos shared album

--- lib/photo_clock.dart
+++ lib/photo_clock.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:intl/intl.dart' as intl; // Must be named as conflicted TextDirection
@@ -6,6 +8,7 @@ import 'package:provider/provider.dart';
 import 'package:smartclock/main.dart' show logger;
 import 'package:smartclock/util/config.dart';
 import 'package:smartclock/util/get_ordinal.dart';
+import 'package:smartclock/util/google_photos_scraper.dart';
 
 class PhotoClock extends StatefulWidget {
   const PhotoClock({super.key, required this.now});
@@ -19,6 +22,7 @@ class PhotoClock extends StatefulWidget {
 class _PhotoClockState extends State<PhotoClock> {
   int photoIndex = 0;
   int thirtyCount = 0;
+  late FutureOr<List<String>> _futureImages;
 
   String get _hour => widget.now.hour == 12 ? "12" : "${widget.now.hour % 12}".padLeft(2, "0");
   // ignore: non_constant_identifier_names
@@ -37,14 +41,33 @@ class _PhotoClockState extends State<PhotoClock> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
+  void initState() {
+    super.initState();
 
-    final config = context.read<ConfigModel>().config;
-    for (final image in config.photos.images) {
-      logger.t("Precaching image: $image");
-      precacheImage(NetworkImage(image), context);
+    FutureOr<List<String>> getImages() async {
+      final config = context.read<ConfigModel>().config;
+
+      List<String> images;
+      if (config.photos.useStaticLinks) {
+        images = config.photos.images;
+      } else {
+        if (config.photos.scraperApiKey.isEmpty || config.photos.googlePhotoAlbumUrl.isEmpty) {
+          logger.w("Cannot scrape Google Photos: API key or album URL is empty");
+          return [];
+        }
+
+        images = await scrapeGooglePhotos(config.photos.scraperApiKey, config.photos.googlePhotoAlbumUrl);
+      }
+
+      for (final image in images) {
+        logger.t("Precaching image: $image");
+        if (mounted) precacheImage(NetworkImage(image), context);
+      }
+
+      return images;
     }
+
+    _futureImages = getImages();
   }
 
   @override
@@ -61,81 +84,89 @@ class _PhotoClockState extends State<PhotoClock> {
         logger.i("Cycling clock image");
         setState(() {
           thirtyCount = 0;
-          photoIndex = (photoIndex + 1) % config.photos.images.length;
+          photoIndex++;
         });
       }
     }
 
-    final image = config.photos.images[photoIndex];
+    return FutureBuilder(
+      future: Future.value(_futureImages),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData && snapshot.connectionState != ConnectionState.done) return const SizedBox.shrink();
 
-    return Container(
-      margin: EdgeInsets.all(config.clock.padding),
-      decoration: BoxDecoration(
-        color: config.sidebar.cardColor,
-        borderRadius: BorderRadius.circular(config.sidebar.cardRadius),
-        image: DecorationImage(
-          image: NetworkImage(image),
-          fit: BoxFit.cover,
-          alignment: Alignment.center,
-        ),
-      ),
-      clipBehavior: Clip.hardEdge,
-      child: Column(
-        children: [
-          const Expanded(flex: 1, child: SizedBox.expand()),
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(100, 0, 0, 0), Colors.transparent],
-                stops: [0, 0.9, 1],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-              ),
-            ),
-            child: Column(
-              textBaseline: TextBaseline.alphabetic,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: config.clock.dateGap),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${config.clock.twentyFourHour ? _24Hour : _hour}:$_minute",
-                      style: TextStyle(fontSize: config.clock.mainSize, height: 0.8, color: Colors.white),
-                      softWrap: false,
-                    ),
-                    if (config.clock.showSeconds) ...[
-                      // Ensure section is always the same width to prevent layout shifts
-                      SizedBox(
-                        width: _textSize(_period, smallStyle).width + config.clock.smallGap,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          verticalDirection: config.clock.twentyFourHour ? VerticalDirection.up : VerticalDirection.down,
-                          children: [
-                            Text(_second, style: smallStyle, softWrap: false),
-                            SizedBox(height: config.clock.smallGap),
-                            Text(!config.clock.twentyFourHour ? _period : "", style: smallStyle, softWrap: false),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ],
-                ),
-                SizedBox(height: config.clock.dateGap),
-                Text(
-                  intl.DateFormat("EEEE d'${getOrdinal(widget.now.day)}' MMMM yyyy").format(widget.now),
-                  style: TextStyle(fontSize: config.clock.dateSize, height: 0.8, color: Colors.white),
-                  textAlign: TextAlign.center,
-                  softWrap: false,
-                ),
-                SizedBox(height: config.clock.dateGap),
-              ],
+        photoIndex %= snapshot.data!.length;
+        final image = snapshot.data![photoIndex];
+
+        return Container(
+          margin: EdgeInsets.all(config.clock.padding),
+          decoration: BoxDecoration(
+            color: config.sidebar.cardColor,
+            borderRadius: BorderRadius.circular(config.sidebar.cardRadius),
+            image: DecorationImage(
+              image: NetworkImage(image),
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
             ),
           ),
-        ],
-      ),
+          clipBehavior: Clip.hardEdge,
+          child: Column(
+            children: [
+              const Expanded(flex: 1, child: SizedBox.expand()),
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(100, 0, 0, 0), Colors.transparent],
+                    stops: [0, 0.9, 1],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                child: Column(
+                  textBaseline: TextBaseline.alphabetic,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: config.clock.dateGap),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "${config.clock.twentyFourHour ? _24Hour : _hour}:$_minute",
+                          style: TextStyle(fontSize: config.clock.mainSize, height: 0.8, color: Colors.white),
+                          softWrap: false,
+                        ),
+                        if (config.clock.showSeconds) ...[
+                          // Ensure section is always the same width to prevent layout shifts
+                          SizedBox(
+                            width: _textSize(_period, smallStyle).width + config.clock.smallGap,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              verticalDirection: config.clock.twentyFourHour ? VerticalDirection.up : VerticalDirection.down,
+                              children: [
+                                Text(_second, style: smallStyle, softWrap: false),
+                                SizedBox(height: config.clock.smallGap),
+                                Text(!config.clock.twentyFourHour ? _period : "", style: smallStyle, softWrap: false),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ],
+                    ),
+                    SizedBox(height: config.clock.dateGap),
+                    Text(
+                      intl.DateFormat("EEEE d'${getOrdinal(widget.now.day)}' MMMM yyyy").format(widget.now),
+                      style: TextStyle(fontSize: config.clock.dateSize, height: 0.8, color: Colors.white),
+                      textAlign: TextAlign.center,
+                      softWrap: false,
+                    ),
+                    SizedBox(height: config.clock.dateGap),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/util/google_photos_scraper.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+Future<List<String>> scrapeGooglePhotos(String apiKey, String photoUrl) async {
+  final url = 'https://scraping.narf.ai/api/v1/?api_key=$apiKey&url=$photoUrl';
+
+  final response = await http.get(Uri.parse(url));
+  final document = parse(response.body);
+
+  final imgElements = document.querySelectorAll('img[src^="https://lh3.googleusercontent.com/pw"]');
+  final imgUrls = imgElements.map((element) => element.attributes['src'] ?? '').where((url) => url.isNotEmpty);
+
+  final dataElements = document.querySelectorAll('[data-latest-bg]');
+  final dataUrls = dataElements.map((element) => element.attributes['data-latest-bg'] ?? '').where((url) => url.isNotEmpty);
+
+  if (imgUrls.isNotEmpty) {
+    return imgUrls.map((e) => e.replaceAll(RegExp(r'=w[0-9]+-h[0-9]+'), "=w1000-h1000")).toList();
+  } else if (dataUrls.isNotEmpty) {
+    return dataUrls.map((e) => e.replaceAll(RegExp(r'=w[0-9]+-h[0-9]+'), "=w1000-h1000")).toList();
+  } else {
+    return [];
+  }
+}
